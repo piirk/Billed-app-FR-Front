e9ffafed4de3b4f8d00398059c7c260580ab9cf4
piirk <baptiste.boucharechas@gmail.com>
2024-09-14 00:44:27 +0200

    Add e2e test for employee journey

---
+++ src/__tests__/e2e/employeeJourney.e2e.test.js
@@ -0,0 +1,15 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { screen } from "@testing-library/dom";
+import Actions from "../../views/Actions.js";
+import '@testing-library/jest-dom';
+
+describe('Given I am connected as an Employee', () => {
+  describe('When ...', () => {
+    it('...', () => {
+
+    });
+  });
+});

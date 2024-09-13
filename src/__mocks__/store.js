c7d9f4dea8227b4150df2b732ccc46fba96c6206
piirk <baptiste.boucharechas@gmail.com>
2024-09-13 15:27:20 +0200

    Refactor create and update methods for Jest

--- src/__mocks__/store.js
+++ src/__mocks__/store.js
@@ -62,26 +62,22 @@ const mockedBills = {
       }])
 
   },
-  create(bill) {
-    return Promise.resolve({fileUrl: 'https://localhost:3456/images/test.jpg', key: '1234'})
-  },
-  update(bill) {
-    return Promise.resolve({
-      "id": "47qAXb6fIm2zOKkLzMro",
-      "vat": "80",
-      "fileUrl": "https://firebasestorage.googleapis.com/v0/b/billable-677b6.a…f-1.jpg?alt=media&token=c1640e12-a24b-4b11-ae52-529112e9602a",
-      "status": "pending",
-      "type": "Hôtel et logement",
-      "commentary": "séminaire billed",
-      "name": "encore",
-      "fileName": "preview-facture-free-201801-pdf-1.jpg",
-      "date": "2004-04-04",
-      "amount": 400,
-      "commentAdmin": "ok",
-      "email": "a@a",
-      "pct": 20
-    })
-  },
+  create: jest.fn(() => Promise.resolve({ fileUrl: 'https://localhost:3456/images/test.jpg', key: '1234' })),
+  update: jest.fn(() => Promise.resolve({
+    "id": "47qAXb6fIm2zOKkLzMro",
+    "vat": "80",
+    "fileUrl": "https://firebasestorage.googleapis.com/v0/b/billable-677b6.a…f-1.jpg?alt=media&token=c1640e12-a24b-4b11-ae52-529112e9602a",
+    "status": "pending",
+    "type": "Hôtel et logement",
+    "commentary": "séminaire billed",
+    "name": "encore",
+    "fileName": "preview-facture-free-201801-pdf-1.jpg",
+    "date": "2004-04-04",
+    "amount": 400,
+    "commentAdmin": "ok",
+    "email": "a@a",
+    "pct": 20
+  }))
 }
 
 export default {

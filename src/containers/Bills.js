6d78f411e371696ee02947cd7c58170c1c755e57
piirk <baptiste.boucharechas@gmail.com>
2024-09-13 12:19:02 +0200

    Refactor date formatting in Bills.js

--- src/containers/Bills.js
+++ src/containers/Bills.js
@@ -1,5 +1,5 @@
 import { ROUTES_PATH } from '../constants/routes.js'
-import { formatDate, formatStatus } from "../app/format.js"
+import { formatStatus } from "../app/format.js"
 import Logout from "./Logout.js"
 
 export default class {
@@ -38,7 +38,6 @@ export default class {
             try {
               return {
                 ...doc,
-                date: doc.date, // formatDate(doc.date),
                 status: formatStatus(doc.status)
               }
             } catch(e) {
@@ -47,7 +46,6 @@ export default class {
               console.log(e,'for',doc)
               return {
                 ...doc,
-                date: doc.date,
                 status: formatStatus(doc.status)
               }
             }
